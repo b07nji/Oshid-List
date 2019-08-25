9467063a3b8434368f21ab75e24c50a8ccaac71f
Tokiya Akiyoshi <tokiya@b07ji.mol>
2019-08-25 16:44:59 +0900

    orgamised constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,12 +4,14 @@ class Constants {
   // Firestore key
   final onegai = 'onegai';
   final users = 'users';
-  // 色
-  final violet = Color.fromRGBO(207, 167, 205, 1);
-  final grey = Color.fromRGBO(229, 229, 229, 1);
+
   // Preference key
   final uuid = 'uuid';
   final userName = 'userName';
   final partnerId = 'partnerId';
   final hasPartner = 'hasPartner';
+
+  // 色
+  final violet = Color.fromRGBO(207, 167, 205, 1);
+  final grey = Color.fromRGBO(229, 229, 229, 1);
 }
