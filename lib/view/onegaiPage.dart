a96383fe14e09995ba9347c0088b29519a1076ea
ryuta7318 <you@example.com>
2019-08-31 03:17:11 +0900

    textfield color change

--- lib/view/onegaiPage.dart
+++ lib/view/onegaiPage.dart
@@ -13,6 +13,7 @@ final _userReference = Firestore.instance.collection(constants.users);
 final user = User();
 final auth = Authentication();
 final constants = Constants();
+
 var partnerName = 'パートナー';
 
 class OnegaiCreator extends StatelessWidget {
@@ -142,14 +143,19 @@ class OnegaiFormState extends State<OnegaiForm> {
         child: ListView(
           children: <Widget>[
             TextFormField(
+              cursorColor:Colors.grey,
               validator: (value) {
                 if (value.isEmpty) return "おねがいを入れてね";
                 return null;
               },
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
-                labelText: 'おねがい'
-              ),
+                labelText: 'おねがい',
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.grey),),
+                focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(color: Colors.deepPurpleAccent),),
+                ),
               onSaved: (value) => (setState(() => _onegai.content = value))
             ),
 
