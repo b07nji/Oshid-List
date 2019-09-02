840ca5e6f08bcd6120b7ad267bf50ff9b8c3e878
Tokiya Akiyoshi <tokiya@b07jimol-6426.local>
2019-09-03 02:29:44 +0900

    add delete notification

--- lib/entity/onegai.dart
+++ lib/entity/onegai.dart
@@ -3,7 +3,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class OnegaiRequest {
   String key;
   String content;
-  String whose;
   String owerRef;
   DateTime dueDate = DateTime.now();
   bool status;
@@ -17,19 +16,14 @@ class OnegaiResponse {
   final DocumentReference reference;
 
   OnegaiResponse.fromMap(Map<String, dynamic> map, {this.reference}) :
-        assert(map['onegaiId'] != null),
-        assert(map['content'] != null),
-        assert((map['dueDate']) != null),
-        assert((map['status']) != null),
+//        assert(map['onegaiId'] != null),
+//        assert(map['content'] != null),
+//        assert((map['dueDate']) != null),
+//        assert((map['status']) != null),
         onegaiId = map['onegaiId'],
         content = map['content'],
         dueDate = DateTime.fromMillisecondsSinceEpoch(map['dueDate'].millisecondsSinceEpoch),
         status = map['status'];
-
-//  Record.fromSnapshot(dynamic snapshot): this.fromMap(
-//      snapshot.data,
-//      reference: snapshot.reference
-//  );
 
   @override
   String toString() => "Record<$content: $dueDate>";
